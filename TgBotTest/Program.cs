3ceeaea800d25add99838b7c4dbeb6831d4e90ed
Alex <pantjuxin-av@yandex.ru>
2024-07-13 15:02:47 +0300

    init

---
+++ TgBotTest/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types.Enums;
+using TgBotTest;
+
+Console.WriteLine("Hello, World!");
+
+const string token = "7125413978:AAEECZ-MQrdGWF8txh7EsLUr6jYo-u-73jw";
+
+// if (token is null) throw new Exception("Bot client token not found");
+
+var botClient = new TelegramBotClient(token);
+ReceiverOptions receiverOptions = new()
+{
+    AllowedUpdates = new[]
+    {
+        UpdateType.Message
+    }, // receive all update types except ChatMember related updates
+};
+
+var updateHandler = new TelegramUpdateHandler();
+
+botClient.StartReceiving(
+    updateHandler: updateHandler.HandleUpdateAsync,
+    pollingErrorHandler: updateHandler.HandlePollingErrorAsync,
+    receiverOptions: receiverOptions,
+    cancellationToken: default
+);
+
+Console.ReadLine();
---
+++ TgBotTest/TelegramUpdateHandler.cs
@@ -0,0 +1,30 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+
+namespace TgBotTest;
+
+public class TelegramUpdateHandler : IUpdateHandler
+{
+    public async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken ct)
+    {
+       // твой код
+    }
+
+    public Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception,
+        CancellationToken cancellationToken)
+    {
+        var errorMessage = exception switch
+        {
+            ApiRequestException apiRequestException
+                => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+            _ => exception.ToString()
+        };
+
+        Console.WriteLine(errorMessage);
+        return Task.CompletedTask;
+    }
+    
+    
+}
